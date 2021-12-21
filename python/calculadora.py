de78cb9f7b65d3b6ae363bcb36fade1ef498f014
Nytrox-x <81092363+Nytrox-x@users.noreply.github.com>
2021-12-21 10:36:38 -0300

    Otimização da calculadora

--- python/calculadora.py
+++ python/calculadora.py
@@ -2,47 +2,48 @@ from tkinter import *
 
 
 class Aplicacao:
+    root = Tk()
+    valor_geral = StringVar()
+    linha1 = ['7', '8', '9', '+']
+    linha2 = ['4', '5', '6', '-']
+    linha3 = ['1', '2', '3', '*']
+    linha4 = ['0', 'C', '=', '/']
+
     def __init__(self):
-        self.janela = Tk()
-        self.valor_geral = StringVar()
         self.valor_geral.set('')
-        self.f = Frame(self.janela, padx=20, pady=20)
-        self.screen = Entry(self.f, textvar=self.valor_geral, font="Lucida 50 bold", bg="light blue")
+        f = Frame(self.root, padx=20, pady=20)
+        self.screen = Entry(f, textvariable=self.valor_geral, font="Lucida 50 bold", bg="light blue")
         self.screen.pack(fill=X, padx=20, pady=15)
-        self.f.pack()
-        self.linha1 = ['7', '8', '9', '+']
-        self.linha2 = ['4', '5', '6', '-']
-        self.linha3 = ['1', '2', '3', '*']
-        self.linha4 = ['0', 'C', '=', '/']
-        self.f = Frame(bg="grey", padx=30, pady=10)
+        f.pack()
+        f = Frame(bg="grey", padx=30, pady=10)
         for i in self.linha1:
-            b = Button(self.f, text=i, padx=10, pady=10, font="Lucida 25 bold")
+            b = Button(f, text=i, padx=10, pady=10, font="Lucida 25 bold")
             b.pack(side=LEFT, padx=10, pady=10)
             b.bind("<Button-1>", self.click)
-        self.f.pack()
-        self.f = Frame(bg="grey", padx=30, pady=10)
+        f.pack()
+        f = Frame(bg="grey", padx=30, pady=10)
         for i in self.linha2:
-            b = Button(self.f, text=i, padx=10, pady=10, font="Lucida 25 bold")
+            b = Button(f, text=i, padx=10, pady=10, font="Lucida 25 bold")
             b.pack(side=LEFT, padx=10, pady=10)
             b.bind("<Button-1>", self.click)
-        self.f.pack()
-        self.f = Frame(bg="grey", padx=30, pady=10)
+        f.pack()
+        f = Frame(bg="grey", padx=30, pady=10)
         for i in self.linha3:
-            b = Button(self.f, text=i, padx=10, pady=10, font="Lucida 25 bold")
+            b = Button(f, text=i, padx=10, pady=10, font="Lucida 25 bold")
             b.pack(side=LEFT, padx=10, pady=10)
             b.bind("<Button-1>", self.click)
-        self.f.pack()
-        self.f = Frame(bg="grey", padx=30, pady=10)
+        f.pack()
+        f = Frame(bg="grey", padx=30, pady=10)
         for i in self.linha4:
-            b = Button(self.f, text=i, padx=10, pady=10, font="Lucida 25 bold")
+            b = Button(f, text=i, padx=10, pady=10, font="Lucida 25 bold")
             b.pack(side=LEFT, padx=10, pady=10)
             b.bind("<Button-1>", self.click)
-        self.f.pack()
-        self.janela.geometry("600x650")
-        self.janela.minsize()
-        self.janela.maxsize()
-        self.janela.config(bg="grey")
-        self.janela.title("Calculadora")
+        f.pack()
+        self.root.geometry("600x650")
+        self.root.minsize()
+        self.root.maxsize()
+        self.root.config(bg="grey")
+        self.root.title("Calculadora")
 
     def click(self, event):
         entrada = event.widget.cget("text")
